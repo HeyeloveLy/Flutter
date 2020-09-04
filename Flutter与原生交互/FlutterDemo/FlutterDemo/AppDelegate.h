02fd5f5720724542e27169c85ae89b14dfbb3dcc
何烨 <49808882+HeyeloveLy@users.noreply.github.com>
2020-09-04 17:20:49 +0800

    原生demo

---
+++ Flutter与原生交互/FlutterDemo/FlutterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlutterDemo
+//
+//  Created by ly-yangliu on 2020/9/3.
+//  Copyright © 2020 swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Flutter/Flutter.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate,FlutterAppLifeCycleProvider>
+
+@property (nonatomic,strong) UIWindow *window;
+
+@end
+
