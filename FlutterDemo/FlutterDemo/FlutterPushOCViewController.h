c04e552a2a7f76dbd7113ab397af490cd05d727f
何烨 <49808882+HeyeloveLy@users.noreply.github.com>
2020-09-07 09:03:45 +0800

    Add files via upload
    上传原生

---
+++ FlutterDemo/FlutterDemo/FlutterPushOCViewController.h
@@ -0,0 +1,20 @@
+//
+//  FlutterPushOCViewController.h
+//  FlutterDemo
+//
+//  Created by ly-yangliu on 2020/9/4.
+//  Copyright © 2020 swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FlutterPushOCViewController : UIViewController
+
+@property(nonatomic, strong)NSString *parames;
+@property (weak, nonatomic) IBOutlet UILabel *targetLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
